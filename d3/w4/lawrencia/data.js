681314cc795556394be7f07d3791fda7814572ca
Jared Whalen <jared.m.whalen@gmail.com>
2024-10-31 12:33:50 -0400

    csv and json load examples

---
+++ d3/w4/lawrencia/data.js
@@ -0,0 +1,50 @@
+const data = [
+  {
+    "month": "January",
+    "value": 2218
+  },
+  {
+    "month": "February",
+    "value": 3031
+  },
+  {
+    "month": "March",
+    "value": 3494
+  },
+  {
+    "month": "April",
+    "value": 3605
+  },
+  {
+    "month": "May",
+    "value": 3491
+  },
+  {
+    "month": "June",
+    "value": 3549
+  },
+  {
+    "month": "July",
+    "value": 2749
+  },
+  {
+    "month": "August",
+    "value": 2465
+  },
+  {
+    "month": "September",
+    "value": 3717
+  },
+  {
+    "month": "October",
+    "value": 3935
+  },
+  {
+    "month": "November",
+    "value": 2406
+  },
+  {
+    "month": "December",
+    "value": 2080
+  }
+]
